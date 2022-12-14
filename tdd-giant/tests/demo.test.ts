df9b5c340dbee1a8162b7ed133a55f44159787d8
Colin CROVILLE <colin.croville@gmail.com>
2022-12-14 22:11:54 +0100

    1st test file

---
+++ tdd-giant/tests/demo.test.ts
@@ -0,0 +1,25 @@
+/*
+Ecrire un programme qui retourne les entiers de 1 à 100
+Avec les contraintes suivantes :
+Pour les multiples de 3, on retourne Fizz à la place.
+Pour les multiples de 5, on retourne Buzz à la place.
+Pour les multiples de 15, on retourne FizzBuzz à la place.
+
+Attendu:
+"12Fizz4BuzzFizz78FizzBuzz......Buzz"
+*/
+import { describe, expect, it } from 'vitest';
+
+describe('FizzBuzz', () => {
+    it('should do nothing', () => {
+        // nothing to do here
+    });
+
+    it('should pass when true is true', () => {
+        expect(true).toBeTruthy();
+    });
+
+    it('should fail when false is true', () => {
+        expect(false).toBeTruthy();
+    });
+})
