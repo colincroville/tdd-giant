b7c531395516d2d6314153a3918ca20660faf9bc
Colin CROVILLE <colin.croville@gmail.com>
2022-12-15 20:30:54 +0100

    solution démo

--- tdd-giant/tests/demo.test.ts
+++ tdd-giant/tests/demo.test.ts
@@ -10,16 +10,109 @@ Attendu:
 */
 import { describe, expect, it } from 'vitest';
 
+class FizzBuzz {
+    generate(start: number, end: number): string {
+        let result = '';
+        
+        for(let i = start; i <= end; i++) {
+            result += this.generateString(i);
+        }
+        
+        return result;
+    }
+
+    private generateString(integer: number): string {
+        if(integer % 15 === 0) {
+            return 'FizzBuzz';
+        }
+
+        if(integer % 5 === 0) {
+            return 'Buzz';
+        }
+
+        if(integer % 3 === 0) {
+            return 'Fizz';
+        }
+
+        return integer.toString();
+    }    
+}
+
 describe('FizzBuzz', () => {
-    it('should do nothing', () => {
-        // nothing to do here
+    it('should return 1 if number is 1', () => {
+        expect(new FizzBuzz().generate(1, 1)).toEqual('1');
     });
 
-    it('should pass when true is true', () => {
-        expect(true).toBeTruthy();
+    it('should return Fizz if number is 3', () => {
+        expect(new FizzBuzz().generate(3, 3)).toEqual('Fizz');
     });
 
-    it('should fail when false is true', () => {
-        expect(false).toBeTruthy();
+    it('should return Buzz if number is 5', () => {
+        expect(new FizzBuzz().generate(5, 5)).toEqual('Buzz');
     });
-})
+
+    it('should return FizzBuzz if number is 15', () => {
+        expect(new FizzBuzz().generate(15, 15)).toEqual('FizzBuzz');
+    });
+
+    it('should return Fizz if number is 27', () => {
+        expect(new FizzBuzz().generate(27, 27)).toEqual('Fizz');
+    });
+
+    it('should return 31 if number is 31', () => {
+        expect(new FizzBuzz().generate(31, 31)).toEqual('31');
+    });
+
+    it('should return Fizz if number is multiple of 3', () => {
+        // Prepare.
+        const fizzBuzz = new FizzBuzz();
+        const testIntegers = [
+            3, 6, 9, 12, 18, 27, 66, 99
+        ];
+
+        // Act.
+        const results = testIntegers.map(x => fizzBuzz.generate(x, x));
+
+        // Assert.
+        expect(results.every(x => x === 'Fizz')).toEqual(true);
+    });
+
+    it('should return Buzz if number is multiple of 5', () => {
+        // Prepare.
+        const fizzBuzz = new FizzBuzz();
+        const testIntegers = [
+            5, 10, 20, 50, 95
+        ];
+
+        // Act.
+        const results = testIntegers.map(x => fizzBuzz.generate(x, x));
+
+        // Assert.
+        expect(results.every(x => x === 'Buzz')).toEqual(true);
+    });
+
+    it('should return FizzBuzz if number is multiple of 15', () => {
+        // Prepare.
+        const fizzBuzz = new FizzBuzz();
+        const testIntegers = [
+            15, 45, 75
+        ];
+
+        // Act.
+        const results = testIntegers.map(x => fizzBuzz.generate(x, x));
+
+        // Assert.
+        expect(results.every(x => x === 'FizzBuzz')).toEqual(true);
+    });
+
+    it('should return numbers from 1 to 100 with multiple replaced', () => {
+        // Prepare.
+        const fizzBuzz = new FizzBuzz();
+
+        // Act.
+        const result = fizzBuzz.generate(1, 100);
+
+        // Assert.
+        expect(result.substring(0, 25)).toEqual('12Fizz4BuzzFizz78FizzBuzz');
+    })
+});
